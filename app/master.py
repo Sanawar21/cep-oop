88c32e8d5937e01623ff221b8626035cf0a12e57
Sanawar_21 <sanawarsaeed@gmail.com>
2024-06-30 03:14:31 +0500

    Add logout route fix

--- app/master.py
+++ app/master.py
@@ -4,10 +4,12 @@ from .blueprints.user_app import UserApp
 from .blueprints.checkout_app import CheckoutApp
 from .blueprints.authenticate_app import AuthenticationApp
 
+from .utils import Paths as paths
+
 
 class MasterApp(Flask):
     def __init__(self):
-        super().__init__(__name__)
+        super().__init__(__name__, static_folder=paths.static)
         self.secret_key = "beKxrYWowAlz"
         self.register_blueprint(AuthenticationApp(), url_prefix="")
         self.register_blueprint(UserApp(), url_prefix="")
